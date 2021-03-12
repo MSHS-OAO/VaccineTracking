771e5701a762a52b96d8daf98ee97911835c993b
kateenevin <kate.nevin@mssm.edu>
2021-03-12 09:17:48 -0500

    Update reference file with 1190 5th Ave pod.

--- Vaccine Sched Repo w Automated Epic Reporting.R
+++ Vaccine Sched Repo w Automated Epic Reporting.R
@@ -58,10 +58,10 @@ update_walkins <- FALSE
 
 # Import reference data for site and pod mappings
 site_mappings <- read_excel(paste0(user_directory, "/ScheduleData/",
-                                   "Automation Ref 2021-03-08.xlsx"),
+                                   "Automation Ref 2021-03-12.xlsx"),
                             sheet = "Site Mappings")
 pod_mappings <- read_excel(paste0(user_directory, "/ScheduleData/",
-                                  "Automation Ref 2021-03-08.xlsx"),
+                                  "Automation Ref 2021-03-12.xlsx"),
                            sheet = "Pod Mappings Simple")
 
 # Store today's date
