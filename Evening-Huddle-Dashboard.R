433a59af5281f25d8231aca8fa86b7f2c53592f7
kateenevin <kate.nevin@mssm.edu>
2021-03-23 17:23:40 -0400

    Update file paths based on new folder names.

--- Evening-Huddle-Dashboard.R
+++ Evening-Huddle-Dashboard.R
@@ -1,5 +1,5 @@
 rmarkdown::render("Evening-Huddle-Stats.Rmd",
                   output_file = paste0(user_directory,
-                                       "/Daily_PM_Huddle_Output/",
+                                       "/Daily PM Huddle Output/",
                                        "MSHS Vaccine Summary ",
                                        format(Sys.Date(), "%m-%d-%y")))
